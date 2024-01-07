868e49dc52b2d1d3efd6be13b64ea7df2338fe55
Maya Letona <mayaletona@Mayas-MacBook-Pro-5.local>
2024-01-07 10:43:25 -0800

    Adding in expo and react native skel :)

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Good Morning Baby!</Text>
+      <Text>Im starting code development today!</Text>
       <StatusBar style="auto" />
     </View>
   );
