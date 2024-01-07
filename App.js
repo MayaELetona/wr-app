58571104872d3667b0cb77b7439c6593f580349d
Maya Letona <mayaletona@Mayas-MacBook-Pro-5.local>
2024-01-07 10:55:33 -0800

    Small text change

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Good Morning Baby!</Text>
-      <Text>Im starting code development today!</Text>
+      <Text>Lets get dressed and get coff?</Text>
       <StatusBar style="auto" />
     </View>
   );
